d6199b34deaed33d384341c23e72d462d8a4194d
Denny_Kate <dennykate22@gmail.com>
2023-02-22 09:10:41 +0630

    change base url

--- helper/functions.js
+++ helper/functions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const BASE_URL = process.env.NEXT_APP_BASE_URL;
+
 export const fetchVideos = async (type, page) => {
   const { data } = await axios.post("http://localhost:3000/api/videos", {
     data: {
